1c0a0d47a775c1023b40e4fb17516c2bda4c531a
UpbeatBeau <70418283+UpbeatBeau@users.noreply.github.com>
2024-02-12 16:22:37 -0500

    Beau Script for Buttons to dissapear
    Added arrays and for loops for buttons

--- Assets/Scripts/Inventory/MenuControl.cs
+++ Assets/Scripts/Inventory/MenuControl.cs
@@ -21,13 +21,18 @@ public class MenuControl : MonoBehaviour
         GameManager.instance.GetComponent<GameManager>().canvas.SetActive(false);
     }
 
+    private void Awake()
+    {
+        menuButtons = new Button[] { equip, use, inspect, combine, drop };
+    }
+
     public Button equip;
     public Button use;
     public Button inspect;
     public Button combine;
     public Button drop;
     public Button[] menuButtons;
-    public List<bool> menuBool;
+    public bool[] menuBool;
 
     [SerializeField] private TMP_Text _itemNameText;
     [SerializeField] private TMP_Text _itemDescriptionText;
@@ -38,16 +43,27 @@ public class MenuControl : MonoBehaviour
         {
             _itemNameText.ClearMesh();
             _itemDescriptionText.ClearMesh();
+            foreach(Button b in menuButtons)
+            {
+                b.gameObject.SetActive(false);
+            }
             return;
         }
 
         _itemNameText.SetText(selectedSlot._itemData.Name);
         _itemDescriptionText.SetText(selectedSlot._itemData.Description[0]);
-        menuBool.Add(selectedSlot._itemData.equip);
-        menuBool.Add(selectedSlot._itemData.use);
-        menuBool.Add(selectedSlot._itemData.inspect);
-        menuBool.Add(selectedSlot._itemData.combine);
-        menuBool.Add(selectedSlot._itemData.drop);
+        menuBool = new bool[] { selectedSlot._itemData.equip, selectedSlot._itemData.use, selectedSlot._itemData.inspect, selectedSlot._itemData.combine, selectedSlot._itemData.drop };
+        for (int i = 0; i < menuButtons.Length; i++)
+        {
+            if (menuBool[i] == true)
+            {
+                menuButtons[i].gameObject.SetActive(true);
+            }
+            else if (menuBool[i] == false)
+            {
+                menuButtons[i].gameObject.SetActive(false);
+            }
+        }
     }
 
 
